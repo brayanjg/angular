442506b5dbd655383099ef9cf3195f1d47c53dad
Brayan Jimenez <brayan.jimenez@epn.edu.ec>
2018-05-08 16:01:09 -0500

    cambiar colores del titulo mediante una funcion

--- spoiler-app-seis/src/app/app.component.ts
+++ spoiler-app-seis/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  // selector o componente
+  templateUrl: './app.component.html', //estructura
+  styleUrls: ['./app.component.css'] //estilos
 })
 export class AppComponent {
-  title = 'app';
+  nombre = 'Adrian';
+  apellido = 'Eguez';
+  edad = 28;
+  sueldo = 1;
+  fechaNacimiento = new Date('1989');
+  nombreBoton = 'CambiarColor';
+  nombreClase = 'sa-titulo';
+  cambiarClaseDeTitulo() {
+  const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+
+    if (nombreClaseEsTitulo){
+      this.nombreClase= 'sa-subtitulo'
+    }
+    else {
+      this.nombreClase= 'sa-titulo'
+    }
+  }
+  aplicarClase(valorInput:string) {
+  this.nombreClase = valorInput;
+  }
 }
+
+
