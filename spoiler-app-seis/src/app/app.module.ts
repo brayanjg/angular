cef81cfd1517c70de78809fa8b99ac0990e651a3
Brayan Jimenez <brayan.jimenez@epn.edu.ec>
2018-05-14 10:42:49 -0500

    produccion y creacion de componentes

--- spoiler-app-seis/src/app/app.module.ts
+++ spoiler-app-seis/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PeliculaBannerComponent } from './peliculas/pelicula-banner/pelicula-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculaBannerComponent
   ],
   imports: [
     BrowserModule
