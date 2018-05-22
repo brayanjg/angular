f3f8f8f6d87795e6dbb96dc83892179b0e74e619
Brayan Jimenez <brayan.jimenez@epn.edu.ec>
2018-05-22 14:09:35 -0500

    eventos

--- spoiler-app-seis/src/app/app.component.ts
+++ spoiler-app-seis/src/app/app.component.ts
@@ -6,26 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'] //estilos
 })
 export class AppComponent {
-  nombre = 'Adrian';
-  apellido = 'Eguez';
-  edad = 28;
-  sueldo = 1;
-  fechaNacimiento = new Date('1989');
-  nombreBoton = 'CambiarColor';
-  nombreClase = 'sa-titulo';
-  cambiarClaseDeTitulo() {
-  const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+  mostrarHola = true;
+  arregloNumeros = [1, 2, 3, 4, 5, 6];
 
-    if (nombreClaseEsTitulo){
-      this.nombreClase= 'sa-subtitulo'
-    }
-    else {
-      this.nombreClase= 'sa-titulo'
-    }
+  arregloPeliculas = [
+    {
+      nombre: 'Tomb Raider',
+      descripcion: 'Alicia Vikander, Walton Goggins',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg'
+    },
+    {
+      nombre: 'Ready Player One',
+      descripcion: 'Tye Sheridan, Olivia Cooke',
+      esEstreno: false,
+      urlImagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzFQprASMSiRnRHYgQl5sHoGr8GiMI9IB2XVIQP_tmD61KFgUY'
+    },
+  ];
+
+
+  alertar(evento) {
+    console.log('Evento: ', evento); // true
+    alert('DIO CLICK EN ES ESTRENO!!!!!!!!!');
   }
-  aplicarClase(valorInput:string) {
-  this.nombreClase = valorInput;
-  }
+
+
+  // nombre = 'Adrian';
+  // apellido = 'Eguez';
+  // edad = 28;
+  // sueldo = 3411234.13;
+  // fechaNacimiento = new Date('1989-06-10');
+  // nombreBoton = 'Cambiar color';
+  // nombreClase = 'sa-titulo';
+  // clase = 'papa';
+  //
+  // cambiarClaseDeTitulo() {
+  //   const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+  //   if (nombreClaseEsTitulo) {
+  //     this.nombreClase = 'sa-subtitulo';
+  //   } else {
+  //     this.nombreClase = 'sa-titulo';
+  //   }
+  // }
+  //
+  // aplicarClase(valorInput: string) {
+  //   this.nombreClase = valorInput;
+  // }
 }
-
 
--- spoiler-app-seis/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ spoiler-app-seis/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -1,22 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-pelicula-banner',
   templateUrl: './pelicula-banner.component.html',
   styleUrls: ['./pelicula-banner.component.css']
 })
-export class PeliculaBannerComponent implements OnInit {
-  urlImagen = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg';
-  descripcionImagen = 'Imagen de la pelicula Tomb Raider';
-  nombrePelicula = 'Tomb Raider';
-  descripcionPelicula = 'Alicia vikander,Walton Googins';
-  esEstreno = true;
+export class PeliculaBannerComponent implements OnInit, OnChanges {
+  @Input() urlImagen: string;
+  @Input() descripcionImagen: string;
+  @Input() nombrePelicula: string;
+  @Input() descripcionPelicula: string;
+  @Input() esEstreno: boolean;
+
+  @Output() dioClickEnEstado: EventEmitter<boolean> = new EventEmitter();
+
+
   textoEstreno: string;
   claseEstreno: string;
 
-  constructor() { }
+  constructor() {
+    // undefined
+  }
 
   ngOnInit() {
+    console.log("Inicio y seteo color", this.nombrePelicula);
+    this.setearColorYClase();
+  }
+
+  ngOnChanges(propiedadesActualizadas) {
+    console.log('Algo', propiedadesActualizadas);
+    if (propiedadesActualizadas.esEstreno) {
+      this.setearColorYClase();
+    }
+  }
+
+  setearColorYClase() {
     if (this.esEstreno) {
       this.textoEstreno = 'Estreno';
       this.claseEstreno = 'sa-color-estado-rosado';
@@ -24,6 +42,10 @@ export class PeliculaBannerComponent implements OnInit {
       this.textoEstreno = 'Proximamente';
       this.claseEstreno = 'sa-color-estado-amarillo';
     }
+  }
+
+  hizoClickEnEstado() {
+    this.dioClickEnEstado.emit(true);
   }
 
 }
